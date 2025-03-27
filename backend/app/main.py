97a56d40e98018dcbb2370cbaeaa7cf3fe845b97
Paritosh Kadam <paritosh1400@gmail.com>
2025-03-27 16:19:53 -0700

    Added Wikipedia endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from .routes.youtube import router as youtube_router
+from .routes.wikipedia import router as wikipedia_router
 
 app = FastAPI()
 app.include_router(youtube_router, prefix="/youtube", tags=["YouTube"])
+app.include_router(wikipedia_router, prefix="/wikipedia", tags=["Wikipedia"])
 
 @app.get("/")
 def home():
