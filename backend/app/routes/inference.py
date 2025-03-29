8be893bbdfba2a62391e3d8df5b7967a1e2bab99
Abhinav Parameshwaran <104693370+Beckendrof@users.noreply.github.com>
2025-03-29 15:05:03 -0700

    Added multiple chat feature to store multi chat histories in the database.

--- backend/app/routes/inference.py
+++ backend/app/routes/inference.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 # from ..services.ollama_service import OllamaService
 from ..services.huggingface_service import HuggingFaceService
 from ..services.vector_store import VectorStore
+from ..models.chat_models import ChatRequest
 
 router = APIRouter()
 
@@ -11,13 +12,18 @@ huggingface_service = HuggingFaceService()
 vector_store = VectorStore()
 
 @router.post("/chat")
-async def chat(prompt: str):
-    """Handle user chat query with history."""
+async def chat(prompt, chat_id: str = ""):
+    """Handle user chat query with history for multiple chats."""
+
     try:
         if not prompt:
             raise HTTPException(status_code=400, detail="Missing 'prompt' in request")
 
-        history = vector_store.get_chat_history()
+        if not chat_id:
+            chat_id = str(uuid4())  # Generate a new chat ID if not provided
+
+        history = vector_store.get_chat_history(chat_id=chat_id)
+        print(history)
         formatted_history = "\n".join(history)
 
         full_prompt = f"""{formatted_history}
@@ -26,9 +32,8 @@ Bot:
 """
         response = huggingface_service.generate_response(full_prompt)
         if response:
-            message_id = str(uuid4())
-            vector_store.add_message(message_id, prompt, response)
+            vector_store.add_message(chat_id, prompt, response)
 
-        return {"response": response}
+        return {"response": response, "chat_id": chat_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
